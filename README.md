659bfcfacfa57517fb556dac36790c502024f453
David VanLaningham <david.vanlaningham@bluemedora.com>
2022-03-31 17:46:07 -0400

    add UI routes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # embeddable-react
+
+This Repo consists of the final version of the code implemented and disccused in [this blog](#todo).
--- api/start.go
+++ api/start.go
@@ -1,11 +1,17 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/observiq/embeddable-react/ui"
+)
 
 func Start() {
 	store := newStore()
 	router := gin.Default()
+
 	addRoutes(router, store)
+	ui.AddRoutes(router)
 
 	router.Run(":4000")
 }
